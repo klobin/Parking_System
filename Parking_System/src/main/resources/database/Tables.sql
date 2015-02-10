da4e573203ef3e3f620e06d3be68fc94ed469f36
TOM <TOM@kLoBin>
2015-02-11 00:05:36 +0530

    Full n final table structure

--- Parking_System/src/main/resources/database/Tables.sql
+++ Parking_System/src/main/resources/database/Tables.sql
@@ -1,9 +1,9 @@
 /********* HSQL db **********/
 
-create table parking_master (floor_no int,bike_count int,car_count int,hv_count int);
-create table car_master (floor_no int,parking_bay varchar(20),parked varchar(20),car_reg_no varchar(20));
-create table bike_master (floor_no int,parking_bay varchar(20),parked varchar(20),bike_reg_no varchar(20));
-create table hv_master (floor_no int,parking_bay varchar(20),parked varchar(20),hv_reg_no varchar(20));
+create table parking_master (floor_no int,bikes int,cars int,hv int);
+create table car_master (floor_id int,parking_bay varchar(20),parked varchar(20),car_reg_no varchar(20));
+create table bike_master (floor_id int,parking_bay varchar(20),parked varchar(20),bike_reg_no varchar(20));
+create table hv_master (floor_id int,parking_bay varchar(20),parked varchar(20),hv_reg_no varchar(20));
 
 
 insert into parking_master values(0,0,0,0);
