cbef00db6987021270a62607307938e3a002cc75
Thomas Francis <thomas.francis@consult.nordea.com>
2015-02-10 15:47:33 +0530

    Added Interface for kontrakt

---
+++ Parking_System/src/main/java/com/parkingsys/connection/Connectkon.java
@@ -0,0 +1,35 @@
+package com.parkingsys.connection;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Connectkon {  
+	private Connection con = null;
+	private InputStream input;
+	Properties properties = new Properties();
+	private String driver;
+	private String connectionURL;
+
+	public Connection giveMeConnection() throws SQLException, IOException{  
+		try{
+			input = Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties");
+			properties.load(input);
+			driver = properties.getProperty("driver");
+			connectionURL = properties.getProperty("url");
+
+			Class.forName(driver);  
+			con=DriverManager.getConnection(connectionURL);
+
+			if(con!=null)
+			{
+				return con;
+			}   
+			System.out.println("Connection not establieshed...!!");
+		}catch(Exception e){ System.out.println(e);}
+		return con;
+	}
+}  
